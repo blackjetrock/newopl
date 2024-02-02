e57791d93f26760f8490b00dd10b70583362da0b
blackrockjet <menadue@gmail.com>
2024-02-02 12:46:39 +0000

    Updates

--- pc/newopl_objdump.c
+++ pc/newopl_objdump.c
@@ -6,15 +6,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-
+#include <string.h>
+#include "newopl.h"
 #include "nopl_obj.h"
 
 FILE *fp;
 
-NOBJ_VAR_SPACE_SIZE    var_space_size;
-NOBJ_QCODE_SPACE_SIZE  qcode_space_size;
-NOBJ_NUM_PARAMETERS    num_parameters;
-NOBJ_PARAMETER_TYPE    parameter_types[NOBJ_MAX_PARAMETERS];
+NOBJ_VAR_SPACE_SIZE      var_space_size;
+NOBJ_QCODE_SPACE_SIZE    qcode_space_size;
+NOBJ_NUM_PARAMETERS      num_parameters;
+NOBJ_PARAMETER_TYPE      parameter_types[NOBJ_MAX_PARAMETERS];
+NOBJ_GLOBAL_VARNAME_SIZE global_varname_size;
 
 int read_item(void *ptr, int n, size_t size)
 {
@@ -53,6 +55,12 @@ void pr_var_space_size(NOBJ_VAR_SPACE_SIZE *x)
 void pr_qcode_space_size(NOBJ_QCODE_SPACE_SIZE *x)
 {
   printf("\nVar Space Size:");
+  pr_uint16(x->size);
+}
+
+void pr_global_varname_size(NOBJ_GLOBAL_VARNAME_SIZE *x)
+{
+  printf("\nGlobal varname Size:");
   pr_uint16(x->size);
 }
 
@@ -107,18 +115,83 @@ int main(int argc, char *argv[])
   if(!read_item((void *)&parameter_types, num_parameters.num, sizeof(NOBJ_PARAMETER_TYPE)))
     {
       printf("\nError reading parameter types.");
-      printf("\nnum=%d sizeof=%ld", num_parameters.num, sizeof(NOBJ_PARAMETER_TYPE));
       return(0);
     }
-    
-    fclose(fp);
 
-    printf("\nDump of @%s'\n", argv[1]);
+  if(!read_item((void *)&global_varname_size, 1, sizeof(NOBJ_GLOBAL_VARNAME_SIZE)))
+    {
+      printf("\nError reading global varname size");
+      return(0);
+    }
 
-    pr_var_space_size(&var_space_size);
-    pr_qcode_space_size(&qcode_space_size);
-    pr_num_parameters(&num_parameters);
-    pr_parameter_types();
-    printf("\n");
+  // Global varname is more complicated to read. Each entry is length
+  // prefixed, so read them until the length we have read matches the
+  // size we have just read.
+
+  int length_read = 0;
+  
+do
+    {
+      uint8_t len;
+      uint8_t varname[NOBJ_VARNAME_MAXLEN];
+      NOBJ_VARTYPE vartype;
+      NOBJ_VARADDR varaddr;
+      vartype = 0;
+      varaddr = 0;
+      
+      if(!read_item((void *)&len, 1, sizeof(len)))
+	{
+	  printf("\nError reading global varname entry length");
+	  return(0);
+	}
+
+      memset(varname, 0, sizeof(varname));
+      
+      printf("\nVarname entry len=%d", len);
+      
+      length_read += sizeof(len);
+
+      // 3 bytes on end are type and addr, so read rest of data into name field
+      
+      if(!read_item((void *)&varname, len, sizeof(uint8_t)))
+	{
+	  printf("\nError reading global varname entry name field");
+	  return(0);
+	}
+      
+      printf("\nvarname='%s'", varname);
+
+      length_read += len;
+
+      // read type and addr
+
+      if(!read_item((void *)&vartype, 1, sizeof(NOBJ_VARTYPE)))
+	{
+	  printf("\nError reading global varname entry type field");
+	  return(0);
+	}
+
+      if(!read_item((void *)&varaddr, 1, sizeof(NOBJ_VARADDR)))
+	{
+	  printf("\nError reading global varname entry addr field");
+	  return(0);
+	}
+
+      printf("\nVarname type=%02X", vartype);
+      printf("\nVaraddr addr=%02X", varaddr);
+      length_read += 3;
+    }
+ while (length_read < global_varname_size.size );  
+  fclose(fp);
+
+  printf("\nDump of @%s'\n", argv[1]);
+
+  pr_var_space_size(&var_space_size);
+  pr_qcode_space_size(&qcode_space_size);
+  pr_num_parameters(&num_parameters);
+  pr_parameter_types();
+  pr_global_varname_size(&global_varname_size);
+
+  printf("\n");
     
 }
--- pc/nopl_obj.h
+++ pc/nopl_obj.h
@@ -3,7 +3,6 @@
 // This is backwards compatible with QCode where possible.
 //
 
-#define NOBJ_MAX_PARAMETERS  255
 typedef uint8_t NOBJ_PARAMETER_TYPE;
 
 typedef struct _NOBJ_VAR_SPACE_SIZE
@@ -20,3 +19,30 @@ typedef struct _NOBJ_NUM_PARAMETERS
 {
   uint8_t num;
 } NOBJ_NUM_PARAMETERS;
+
+typedef struct _NOBJ_GLOBAL_VARNAME_SIZE
+{
+  uint16_t size;
+} NOBJ_GLOBAL_VARNAME_SIZE;
+
+typedef uint8_t NOBJ_VARTYPE;
+
+enum
+  {
+   NOBJ_VARTYPE_INT    = 0x00,
+   NOBJ_VARTYPE_FLT    = 0x01,
+   NOBJ_VARTYPE_STR    = 0x02,
+   NOBJ_VARTYPE_INTARY = 0x03,
+   NOBJ_VARTYPE_FLTARY = 0x04,
+   NOBJ_VARTYPE_STRARY = 0x05,
+  };
+
+typedef uint16_t NOBJ_VARADDR;
+
+typedef struct _NOBJ_VARNAME_ENTRY
+{
+  uint8_t len;
+  uint8_t varname[NOBJ_VARNAME_MAXLEN];
+  NOBJ_VARTYPE  type;
+  NOBJ_VARADDR  address;
+} NOBJ_VARNAME_ENTRY;
