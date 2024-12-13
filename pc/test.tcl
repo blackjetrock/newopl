7fd12bee97ad31f588181bd251cb0bb23b3a6e2a
menadue <menadue@gmail.com>
2024-12-13 05:18:31 +0000

    Update

--- pc/test.tcl
+++ pc/test.tcl
@@ -21,18 +21,29 @@ proc compare_results {basename f1 f2} {
 
     set identical 1
 
+    set lineno 0
+    
     foreach l1 [split $f1txt "\n"] l2 [split $f2txt "\n"] {
 	if { [string first "Filename" $l1] != -1 } {
+	    set line_identical($lineno) 1
 	    continue
 	}
 
 	if { [string first "File ext" $l1] != -1 } {
+	    set line_identical($lineno) 1
 	    continue
 	}
 	
 	if { [string compare $l1 $l2] != 0 } {
 	    set identical 0
+	    set line_identical($lineno) 0
+	} else {
+	    
+	    # Identical, mark as such
+	    set line_identical($lineno) 1
 	}
+	
+	incr lineno 1
     }
 
     if { $identical } {
@@ -42,8 +53,33 @@ proc compare_results {basename f1 f2} {
 	set r " not identical"
 	incr ::FAIL 1
     }
+
+    # Now find blocks of non-identical lines
+    set last_state 1
+    set num_diffblocks 0
     
-    puts [format "%-20s   %s" $basename\.opl $r]
+    for {set i 0} {$i<$lineno} {incr i 1} {
+	if { [set line_identical($i)] && !$last_state } {
+	    set diffblock_end($num_diffblocks) $i
+	    incr num_diffblocks 1
+	}
+
+	if { !$line_identical($i) && $last_state } {
+	    set diffblock_start($num_diffblocks) $i
+	}
+
+	set last_state $line_identical($i)
+    }
+
+    puts -nonewline [format "%-20s   %-14s" $basename\.opl $r]
+    
+    puts -nonewline " "
+    foreach db [array names diffblock_start] {
+	puts -nonewline "[set diffblock_start($db)]-[set diffblock_end($db)]  "
+    }
+    puts ""
+    
+
 }
 
 
