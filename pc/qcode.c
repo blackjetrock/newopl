71812ffc5105550c9940c9e3c6783e4392cce9ef
menadue <menadue@gmail.com>
2025-01-02 07:41:55 +0000

    Update

--- pc/qcode.c
+++ pc/qcode.c
@@ -1035,6 +1035,42 @@ void qca_sqr_num(NOBJ_MACHINE *m, NOBJ_QCS *s)
 
 //------------------------------------------------------------------------------
 
+void qca_int_to_num(NOBJ_MACHINE *m, NOBJ_QCS *s)
+{
+  // Convert int to float form
+
+  // Check float isn't too big
+  
+  NOPL_INT res;
+  NOPL_FLOAT *n = &(s->num);
+  
+  if( s->num.exponent > 4 )
+    {
+      runtime_error("Float too big");
+    }
+  
+  if( NUM_IS_POSITIVE(n) )
+    {
+      
+    }
+  else
+    {
+    }
+  
+}
+
+void qca_num_to_int(NOBJ_MACHINE *m, NOBJ_QCS *s)
+{
+  // Convert float to int.
+  // This always succeeds
+  NOPL_INT res;
+
+  
+  
+}
+
+//------------------------------------------------------------------------------
+
 void qca_add_num(NOBJ_MACHINE *m, NOBJ_QCS *s)
 {
   num_add(&(s->num2), &(s->num), &(s->num_result));
@@ -1112,6 +1148,26 @@ void qca_goto(NOBJ_MACHINE *m, NOBJ_QCS *s)
 
   m->rta_pc += offset -2;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//
+
+void qca_chr(NOBJ_MACHINE *m, NOBJ_QCS *s)
+{
+  // We have an int, build a string that has one character with the ascii code
+  // of the int.
+
+  s->str[0] = (s->integer) & 0xFF;
+  s->len = 1;
+}
+
+//------------------------------------------------------------------------------
+
+void qca_push_string(NOBJ_MACHINE *m, NOBJ_QCS *s)
+{
+  push_machine_string(m, s->len, s->str);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -1255,8 +1311,8 @@ NOBJ_QCODE_INFO qcode_info[] =
     { QCO_DROP_WORD,     "QCO_DROP_WORD",     {qca_pop_int,      qca_null,        qca_null}},
     { QCO_DROP_NUM,      "QCO_DROP_NUM",      {qca_pop_num,      qca_null,        qca_null}},
     { QCO_DROP_STR,      "QCO_DROP_STR",      {qca_pop_str,      qca_null,        qca_null}},
-    // QCO_INT_TO_NUM          0x86    
-    // QCO_NUM_TO_INT          0x87    
+    { QCO_INT_TO_NUM,    "QCO_INT_TO_NUM",    {qca_pop_int,      qca_int_to_num,  qca_push_num_result}},
+    { QCO_NUM_TO_INT,    "QCO_NUM_TO_INT",    {qca_pop_num,      qca_num_to_int,  qca_push_result}},
     // QCO_END_FIELDS          0x88    
     // QCO_RUN_ASSEM           0x89    
     { RTF_ADDR,          "RTF_ADDR",          {qca_pop_ref,      qca_null,        qca_null}},
@@ -1305,7 +1361,8 @@ NOBJ_QCODE_INFO qcode_info[] =
     // RTF_VAL                 0xB5    
     // RTF_SPACE               0xB6    
     // RTF_DIR                 0xB7    
-    // RTF_CHR                 0xB8    
+    // RTF_CHR                 0xB8
+    { RTF_CHR,           "RTF_CHR",           {qca_pop_int,      qca_chr,         qca_push_string}},
     // RTF_DATIM               0xB9    
     // RTF_SERR                0xBA    
     // RTF_FIX                 0xBB    
