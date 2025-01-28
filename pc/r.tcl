8d9c4ac68342ed9bc79fa8158021370ec3da598d
Andrew Menadue <menadue@li-8a1a7c4c-2cad-11b2-a85c-98676a60fc93.ibm.com>
2025-01-28 14:15:06 +0000

    Updates

--- pc/r.tcl
+++ pc/r.tcl
@@ -32,7 +32,11 @@ proc pr {} {
     
 }
 
-set ::N "000000000001"
+
+set ::N "00000000000001"
+set ::N "22222222222222"
+set ::N "12345678901234"
+
 
 proc daa {x} {
     if { $x > 9 } {
@@ -43,25 +47,54 @@ proc daa {x} {
 }
 
 proc rnd {x} {
+    #puts "n:'$::N' len:[string length $::N]"
 
     set i 0
-
-    while { $i < 12 } {
-	set d($i) [string range $x $i $i] 
+    while { $i < 14 } {
+	set d($i) [string range $::N $i $i]
+	incr i 1
     }
     
-    puts "$d(0) $d(6)"
-
     set i 0
-    while {$i <48 } {
-	set digits0 [expr $d0 + (($d0 ^ $d6) & 1)]
-	puts $nd0
+    while {$i < 48 } {
+	set d(0) [expr $d(0) + (($d(4) ^ $d(13)) & 1)]
 
+	foreach nm [array names d] {
+	    #puts -nonewline "$d($nm) "
+	}
+	#puts ""
+
+	set j 0
+	while { $j < 14 } {
+	    
+	    if { $d($j) > 9 } {
+		set d($j) [expr $d($j)-10]
+		incr d([expr $j+1]) 1
+	    }
+
+	    incr j 1
+	}
 	
 	incr i 1
     }
+
+    foreach nm [array names d] {
+	#puts -nonewline "$d($nm) "
+    }
+    #puts ""
+
+    set ::N ""
+    foreach nm [array names d] {
+	
+	set ::N "$::N$d($nm)"
+    }
+puts "N='$::N'"
+    
+    
 }
 
-
-rnd $::N
-
+set i 0
+while { $i < 1000 } {
+    rnd $::N
+    incr i 1
+}
