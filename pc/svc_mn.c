e431ddcb9bebcbf2ade5c3b07ba00ef89a60f8dc
menadue <menadue@gmail.com>
2025-02-04 20:07:01 +0000

    Update

---
+++ pc/svc_mn.c
@@ -0,0 +1,11 @@
+
+
+#include "svc_mn.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Uses menustr to put a menu on the display, returns the index of
+// the selected item, or -1 if menu was exited
+
+int mn_disp(char *menustr, 
