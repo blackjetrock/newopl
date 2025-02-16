690f31e13d737344d7fa90fd0e0fdcbf49948156
menadue <menadue@gmail.com>
2025-02-16 10:13:17 +0000

    Update

--- pc/qcode.c
+++ pc/qcode.c
@@ -2352,6 +2352,7 @@ void qca_goto(NOBJ_MACHINE *m, NOBJ_QCS *s)
 
 void qca_chr(NOBJ_MACHINE *m, NOBJ_QCS *s)
 {
+  //
   // We have an int, build a string that has one character with the ascii code
   // of the int.
 
@@ -2361,32 +2362,26 @@ void qca_chr(NOBJ_MACHINE *m, NOBJ_QCS *s)
 
 void qca_fix(NOBJ_MACHINE *m, NOBJ_QCS *s)
 {
-  NOPL_FLOAT f;
   int w, decpl;
   
   // Pop arguments
-  decpl = pop_machine_int(m);
   w = pop_machine_int(m);
-
+  decpl = pop_machine_int(m);
   qca_pop_num(m, s);
-  
-  f = s->num;
 
+  num_round_up(&(s->num), decpl);
+  
   // Convert float to string
   strcpy(s->str, num_to_text(&(s->num)));
+
+  //printf("\nw=%d, dp=%d len=%d, s=%s", w, decpl, s->len, s->str);
+  num_set_decimal_places(s->str, decpl);
+    
+  //printf("\nw=%d, dp=%d len=%d, s=%s\n", w, decpl, s->len, s->str);
+
+  num_force_str_to_width(s->str, w);
+
   s->len = strlen(s->str);
-
-  printf("\nw=%d, l=%d, s=%s", w, s->len, s->str);
-  
-  if( s->len > w )
-    {
-      s->len = w;
-
-      for(int i= 0; i< s->len; i++)
-	{
-	  s->str[i] = '*';
-	}
-    }
 }
 
 void qca_gen(NOBJ_MACHINE *m, NOBJ_QCS *s)
